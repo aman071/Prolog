306bb2f8eaf4f518f2a41e46459ee0bf5fe23940
aman071 <amanrehman18@gmail.com>
2020-09-06 15:34:24 +0530

    added husband, wife, son, daughter, brother, sister, siblings, brother-in-law, sister-in-law, uncle, aunt

--- facts.pl
+++ facts.pl
@@ -27,4 +27,32 @@ is_grandmother(X,Y) :- parent(X,Z), parent(Z,Y), female(X).
 
 is_firstcousin(X,Y) :- is_grandfather(Z,X), is_grandfather(W,Y), Z=W.
 
+is_husband(X,Y) :- spouse(X,Y), male(X).
+
+is_wife(Y,X) :- spouse(X,Y), female(Y).
+
+is_son(X,Y) :- parent(Y,X), male(X).
+
+is_daughter(X,Y) :- parent(Y,X), female(X).
+
+is_brother(X,Y) :- parent(Z,X), parent(W,Y), Z=W, male(X).
+
+is_sister(X,Y) :- parent(Z,X), parent(W,Y), Z=W, female(X).
+
+siblings(X,Y) :- is_brother(X,Y) ; is_sister(X,Y).
+
+is_brotherinlaw(X,Z) :- spouse(Y,Z), is_brother(X,Y) , male(X).
+
+is_sisterinlaw(X,Z) :- spouse(Y,Z), is_sister(X,Y) , female(X).
+
+is_uncle(X,Y) :- parent(Z,Y), is_brother(X,Z).
+
+is_aunt(X,Y) :- parent(Z,Y), is_sister(X,Z).
+
+
+
+
+
+
+
 
