ebfdc685ee20d49849ee37454b6007c6fb83e589
aman071 <amanrehman18@gmail.com>
2020-09-19 12:41:30 +0530

    listed branches, condition working

--- Career-Advisory/career.pl
+++ Career-Advisory/career.pl
@@ -1,8 +1,16 @@
 welcome:-
-    write('Welcome to AI Career Counselor'),nl,
-    write('Please enter your branch first: '),
-    read(Branch_id),
+    nl,write('Welcome to AI Career Counselor'),nl,
+    write('Please enter your branch from the list.'), nl, list_branches,
+    nl,write('Enter number here: '),read(Branch_id),
     refer(Branch_id).
+
+
+list_branches:-
+    write('0: CSE'),nl,
+    write('1: ECE'),nl,
+    write('2: CSD'),nl,
+    write('3: CSSS'),nl.
+
 
 refer(Branch_id):-
     (   Branch_id =:= 0 ->
@@ -22,5 +30,4 @@ refer(Branch_id):-
     ).
 
 cse :-
-    write('I will ask a set of questions, please answer them 0 for No, 1 for Yes'),
-
+    write('I will ask a set of questions, answer them 0 for No, 1 for Yes').
