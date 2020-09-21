d373f0919a0b1e3cda98654b1e1e7094fb3fc01b
aman071 <amanrehman18@gmail.com>
2020-09-21 23:55:33 +0530

    added recommend

--- Career-Advisory/career.pl
+++ Career-Advisory/career.pl
@@ -1,7 +1,20 @@
 cls :- write('\e[2J').
 
+add(X,List,[X|List]).
+
+/*    debugging, coding, database, web designging, problem solver, logical thinking, problem solving, working with data,
+    imagination, creativity, design thinking, user experience, ui design, dealing with people, talking to people, market research
+    hardware, processors, circuits,
+*/
+
+recommend(W, X, Y, Z,O)		:- (W == 1, X == 1, Y==1, Z==0), O = 'CSD might suit you.',nl.
+recommend(W, X, Y, Z,O)		:- (W == 1, X == 0, Y==1, Z==0), O = 'CSSS might suit you.',nl.
+recommend(W, X, Y, Z,O)		:- (W == 0, X == 0, Y==0, Z==1), O = 'ECE might suit you.',nl.
+recommend(W, X, Y, Z,O)		:- (W == 1, X == 0, Y==0, Z==0), O = 'CSE might suit you.',nl.
+
 welcome:-
     nl,write('Welcome to AI Career Counselor'),nl,
+    write('Enter your roll number: '),read(Roll),nl,
     write('Please enter your branch from the list.'), nl, list_branches,
     nl,write('Enter number here: '),read(Branch_id),
     refer(Branch_id).
@@ -34,4 +47,9 @@ refer(Branch_id):-
     ).
 
 cse :-
-    write('I will ask a set of questions, answer them 0 for No, 1 for Yes').
+    write('We will ask you a set of questions, answer them 0 for No, 1 for Yes'), nl,
+    write('Would you say you are very proficient at coding? '), read(Code),
+    write('Would you say you are very proficient at designing? '), read(Des),
+    write('Would you say you are very proficient at Social Science? '), read(Soc),
+    write('Would you say you are very proficient at Electronics? '), read(Elec),
+    recommend(Code, Des, Soc, Elec, Out), write(Out),nl.
