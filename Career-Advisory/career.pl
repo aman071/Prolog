a10f19864138529c8196da7519575f0fbdfb6fab
aman071 <amanrehman18@gmail.com>
2020-09-22 23:21:27 +0530

    added cse career check, checked likes asserts

--- Career-Advisory/career.pl
+++ Career-Advisory/career.pl
@@ -14,7 +14,7 @@ welcome:-
     write('Enter your roll number: '),read(Roll), asserta(getRoll(Roll)),nl,
     write('Please enter your branch from the list.'), nl, list_branches,
     nl,write('Enter number here: '),read(Branch_id),
-    refer(Branch_id, Roll),
+    refer(Branch_id),
     retractall(likes(Roll,_,_)).
 
 
@@ -22,23 +22,19 @@ refer(Branch_id):-
     nth0(Branch_id, [cse, ece, csd, csss], Elem),
     (   Branch_id =:= 0 ->
             format('You chose ~w', [Elem]),nl,nl,
-            getRoll(X),
-            general_interest(Elem, X)
+            general_interest(Elem)
             ;
         Branch_id =:= 1 ->
             format('You chose ~w', [Elem]),nl,nl,
-            getRoll(X),
-            general_interest(Elem, X)
+            general_interest(Elem)
             ;
         Branch_id =:= 2 ->
             format('You chose ~w', [Elem]),nl,nl,
-            getRoll(X),
-            general_interest(Elem, X)
+            general_interest(Elem)
             ;
         Branch_id =:= 3 ->
             format('You chose ~w', [Elem]),nl,nl,
-            getRoll(X),
-            general_interest(Elem, X)
+            general_interest(Elem)
             ;
             write('Incorrect option.'),
             nl,fail
@@ -57,8 +53,6 @@ recommend(W, X, Y, Z, Roll, O)		:- (W == 1, X == 1, Y==1, Z==0), O = 'CSD might 
 recommend(W, X, Y, Z, Roll, O)		:- (W == 1, X == 1, Y==1, Z==0), O = 'CSD might suit you. ',
                                            asserta(likes(Roll,code)), nl.
 
-
-
 general_interest(From_branch):-
     write('We will ask you a set of questions, answer them 0 for No, 1 for Yes'), nl,
     write('Would you say you are very proficient at coding? '), read(Code),
@@ -66,15 +60,14 @@ general_interest(From_branch):-
     write('Would you say you are very interested Social Science? '), read(Soc),
     write('Would you say you are very proficient at Electronics? '), read(Elec),
     getRoll(Roll),
-    recommend(Code, Des, Soc, Elec, Roll, Out), write(Out), format('Would you like to explore that or go with ~w? ',[From_branch]),
+    recommend(Code, Des, Soc, Elec, Roll, Out), write(Out), format('Would you like to explore that or go with ~w? ',[From_branch]), nl,
     write('Enter the branch in lower case: '), read(Choice),nl,
     (  Choice == 'csd' ->
             write(' Going to CSD careers'),nl,
             csd
             ;
-            go_to(From_branch),
-            format(' Going forward with ~w'),
-            cse
+            format('Going forward with ~w',[From_branch]),nl,nl,
+            go_to(From_branch)
     ).
 
 
@@ -89,17 +82,23 @@ go_to(From_branch):- From_branch == 'csss', csss.
 go_to(From_branch):- From_branch == 'ece', ece.
 
 
-add_knowledge(What,Answer):- Answer==1, getRoll(X), assert(likes(X,What,Answer)).
+add_knowledge(What,Answer):- assert(likes(What,Answer)).
 
 /*debugging, coding, database, web designging, problem solver, logical thinking, problem solving, working with data,
     imagination, creativity, design thinking, user experience, ui design, dealing with people, talking to people, market research
     hardware, processors, circuits,
 */
 
+check_cse_career :-
+    likes(debug,1),likes(code,1),likes(problem_solving,1),likes(data_analyst,1),
+    write('You are a good fit for the following careers: '),nl.
+
+
 cse :-
     write('We will ask you a set of questions, answer them 0 for No, 1 for Yes'), nl,
-    write('Do you like debugging code? '), read(Answer), add_knowledge(debug, Answer), nl,
-    write('Do you like coding? '), read(Answer), add_knowledge(code, Answer), nl,
-    write('Do you like database management? '), read(Answer),  add_knowledge(database, Answer), nl,
-    write('Do you like problem solving? '), read(Answer),  add_knowledge(problem_solving, Answer), nl,
-    write('Do you like working with data? '), read(Answer), add_knowledge(data_analyst, Answer), nl.
+    write('Do you like debugging code? '), read(Answer1), add_knowledge(debug, Answer1), nl,
+    write('Do you like coding? '), read(Answer2), add_knowledge(code, Answer2), nl,
+    write('Do you like database management? '), read(Answer3),  add_knowledge(database, Answer3), nl,
+    write('Do you like problem solving? '), read(Answer4),  add_knowledge(problem_solving, Answer4), nl,
+    write('Do you like working with data? '), read(Answer5), add_knowledge(data_analyst, Answer5), nl,
+    write('Do you like Machine Learning? '), read(Answer6), add_knowledge(data_analyst, Answer6), nl.
