7b1de3f633bc7fbba4d5e8c2001038509a0d5aad
aman071 <amanrehman18@gmail.com>
2020-09-22 13:01:17 +0530

    added general, go_to

--- Career-Advisory/career.pl
+++ Career-Advisory/career.pl
@@ -7,10 +7,10 @@ add(X,List,[X|List]).
     hardware, processors, circuits,
 */
 
-recommend(W, X, Y, Z,O)		:- (W == 1, X == 1, Y==1, Z==0), O = 'CSD might suit you.',nl.
-recommend(W, X, Y, Z,O)		:- (W == 1, X == 0, Y==1, Z==0), O = 'CSSS might suit you.',nl.
-recommend(W, X, Y, Z,O)		:- (W == 0, X == 0, Y==0, Z==1), O = 'ECE might suit you.',nl.
-recommend(W, X, Y, Z,O)		:- (W == 1, X == 0, Y==0, Z==0), O = 'CSE might suit you.',nl.
+recommend(W, X, Y, Z,O)		:- (W == 1, X == 1, Y==1, Z==0), O = 'CSD might suit you. ',nl.
+recommend(W, X, Y, Z,O)		:- (W == 1, X == 0, Y==1, Z==0), O = 'CSSS might suit you. ',nl.
+recommend(W, X, Y, Z,O)		:- (W == 0, X == 0, Y==0, Z==1), O = 'ECE might suit you. ',nl.
+recommend(W, X, Y, Z,O)		:- (W == 1, X == 0, Y==0, Z==0), O = 'CSE might suit you. ',nl.
 
 welcome:-
     nl,write('Welcome to AI Career Counselor'),nl,
@@ -31,7 +31,7 @@ refer(Branch_id):-
     nth0(Branch_id, [cse, ece, csd, csss], Elem),
     (   Branch_id =:= 0 ->
             format('You chose ~w', [Elem]),nl,nl,
-            cse
+            cse(Elem)
             ;
         Branch_id =:= 1 ->
             format('You chose ~w', [Elem]),nl,nl
@@ -46,10 +46,55 @@ refer(Branch_id):-
             nl,fail
     ).
 
-cse :-
+general_interest(From_branch):-
     write('We will ask you a set of questions, answer them 0 for No, 1 for Yes'), nl,
     write('Would you say you are very proficient at coding? '), read(Code),
-    write('Would you say you are very proficient at designing? '), read(Des),
-    write('Would you say you are very proficient at Social Science? '), read(Soc),
+    write('Do you like designing? '), read(Des),
+    write('Would you say you are very interested Social Science? '), read(Soc),
     write('Would you say you are very proficient at Electronics? '), read(Elec),
-    recommend(Code, Des, Soc, Elec, Out), write(Out),nl.
+    recommend(Code, Des, Soc, Elec, Out), write(Out), format('Would you like to explore that or go with ~w? ',From_Branch),
+    write('Enter the branch in lower case: '), read(Choice),nl,
+    (  Choice == 'csd' ->
+            write(' Going to CSD careers'),nl,
+            csd
+            ;
+            go_to(From_Branch),
+            format(' Going forward with ~w'),
+            cse
+    ).
+
+go_to(From_Branch):- From_Branch == 'cse', cse.
+go_to(From_Branch):- From_Branch == 'csd', csd.
+go_to(From_Branch):- From_Branch == 'csss', csss.
+go_to(From_Branch):- From_Branch == 'ece', ece.
+
+
+
+/*debugging, coding, database, web designging, problem solver, logical thinking, problem solving, working with data,
+    imagination, creativity, design thinking, user experience, ui design, dealing with people, talking to people, market research
+    hardware, processors, circuits,
+*/
+
+cse :-
+    write('We will ask you a set of questions, answer them 0 for No, 1 for Yes'), nl.
+
+
+
+
+
+
+csd(Chosen_branch) :-
+    write('We will ask you a set of questions, answer them 0 for No, 1 for Yes'), nl,
+    write('Would you say you are very proficient at coding? '), read(Code),
+    write('Do you like designing? '), read(Des),
+    write('Would you say you are very interested Social Science? '), read(Soc),
+    write('Would you say you are very proficient at Electronics? '), read(Elec),
+    recommend(Code, Des, Soc, Elec, Out), write(Out), write('Would you like to explore that or go with your branch? '),
+    write('Enter the branch in lower case: '), read(Choice),nl,
+    (  Choice == 'csd' ->
+            write(' Going to CSD careers'),nl,
+            csd_only
+            ;
+            write(' Going forward with CSE.'),
+            cse_only
+    ).
