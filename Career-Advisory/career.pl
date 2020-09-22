ecc444d5828f7e894ac753a6f1eb929631cb7dc7
aman071 <amanrehman18@gmail.com>
2020-09-22 13:15:41 +0530

    added assert

--- Career-Advisory/career.pl
+++ Career-Advisory/career.pl
@@ -7,17 +7,25 @@ add(X,List,[X|List]).
     hardware, processors, circuits,
 */
 
-recommend(W, X, Y, Z,O)		:- (W == 1, X == 1, Y==1, Z==0), O = 'CSD might suit you. ',nl.
-recommend(W, X, Y, Z,O)		:- (W == 1, X == 0, Y==1, Z==0), O = 'CSSS might suit you. ',nl.
-recommend(W, X, Y, Z,O)		:- (W == 0, X == 0, Y==0, Z==1), O = 'ECE might suit you. ',nl.
-recommend(W, X, Y, Z,O)		:- (W == 1, X == 0, Y==0, Z==0), O = 'CSE might suit you. ',nl.
+/* code, des, social, electronics */
+recommend(W, X, Y, Z, Roll, O)		:- (W == 1, X == 1, Y==1, Z==0), O = 'CSD might suit you. ',
+                                           asserta(likes(Roll,code)),asserta(likes(Roll,des)),asserta(likes(Roll,social)), nl.
+
+recommend(W, X, Y, Z, Roll, O)		:- (W == 1, X == 1, Y==1, Z==0), O = 'CSD might suit you. ',
+                                           asserta(likes(Roll,code)),asserta(likes(Roll,social)), nl.
+
+recommend(W, X, Y, Z, Roll, O)		:- (W == 1, X == 1, Y==1, Z==0), O = 'CSD might suit you. ',
+                                           asserta(likes(Roll,social)), nl.
+
+recommend(W, X, Y, Z, Roll, O)		:- (W == 1, X == 1, Y==1, Z==0), O = 'CSD might suit you. ',
+                                           asserta(likes(Roll,code)), nl.
 
 welcome:-
     nl,write('Welcome to AI Career Counselor'),nl,
     write('Enter your roll number: '),read(Roll),nl,
     write('Please enter your branch from the list.'), nl, list_branches,
     nl,write('Enter number here: '),read(Branch_id),
-    refer(Branch_id).
+    refer(Branch_id, Roll).
 
 
 list_branches:-
@@ -27,11 +35,11 @@ list_branches:-
     write('3: CSSS'),nl.
 
 
-refer(Branch_id):-
+refer(Branch_id, Roll):-
     nth0(Branch_id, [cse, ece, csd, csss], Elem),
     (   Branch_id =:= 0 ->
             format('You chose ~w', [Elem]),nl,nl,
-            cse(Elem)
+            general_interest(Elem, Roll)
             ;
         Branch_id =:= 1 ->
             format('You chose ~w', [Elem]),nl,nl
@@ -46,27 +54,27 @@ refer(Branch_id):-
             nl,fail
     ).
 
-general_interest(From_branch):-
+general_interest(From_branch, Roll):-
     write('We will ask you a set of questions, answer them 0 for No, 1 for Yes'), nl,
     write('Would you say you are very proficient at coding? '), read(Code),
     write('Do you like designing? '), read(Des),
     write('Would you say you are very interested Social Science? '), read(Soc),
     write('Would you say you are very proficient at Electronics? '), read(Elec),
-    recommend(Code, Des, Soc, Elec, Out), write(Out), format('Would you like to explore that or go with ~w? ',From_Branch),
+    recommend(Code, Des, Soc, Elec, Roll, Out), write(Out), format('Would you like to explore that or go with ~w? ',[From_branch]),
     write('Enter the branch in lower case: '), read(Choice),nl,
     (  Choice == 'csd' ->
             write(' Going to CSD careers'),nl,
             csd
             ;
-            go_to(From_Branch),
+            go_to(From_branch),
             format(' Going forward with ~w'),
             cse
     ).
 
-go_to(From_Branch):- From_Branch == 'cse', cse.
-go_to(From_Branch):- From_Branch == 'csd', csd.
-go_to(From_Branch):- From_Branch == 'csss', csss.
-go_to(From_Branch):- From_Branch == 'ece', ece.
+go_to(From_branch):- From_branch == 'cse', cse.
+go_to(From_branch):- From_branch == 'csd', csd.
+go_to(From_branch):- From_branch == 'csss', csss.
+go_to(From_branch):- From_branch == 'ece', ece.
 
 
 
