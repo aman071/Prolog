7f23cc51d42dd201a0138f68a879a31293c72ec9
aman071 <amanrehman18@gmail.com>
2020-09-23 16:22:50 +0530

    adding ece careers

--- Career-Advisory/career.pl
+++ Career-Advisory/career.pl
@@ -191,7 +191,8 @@ check_csss_career :-
     write('You are a good fit for the following careers: '),nl,
     write('1. Survey Researcher: Survey researchers find employment in academic institutions, nonprofit organizations, corporations, research firms, government agencies and polling organizations. The surveys they create may cover a wide range of topics, from scientific to political and social to economic. Survey researchers are professionals who specialize in creating quality surveys and interpreting the information that results. '), nl,
     write('2. Data Warehouse Specialist: A data warehouse specialist works with teams of computer gurus, designing and implementing systems that solve specific client problems. You also make sure the system is maintained, reconciled, updated, and backed up as needed.'), nl,
-    write('3. Data Architect: Data Architects ensure the complete and successful building and integration of data systems and all of their components. With careful architecture, they ensure the implementation, organization, and reliability of those systems.'), nl.
+    write('3. Data Architect: Data Architects ensure the complete and successful building and integration of data systems and all of their components. With careful architecture, they ensure the implementation, organization, and reliability of those systems.'), nl,
+    write('4. Human-Centered Machine Learning Designer: Human-Centered Machine Learning Designer develops various systems that can perform Human Centered Machine Learning based on information processing and pattern recognition. This allows the machine to “learn” the preferences of individual humans.'), nl.
 
 csss :-
     write('We will ask you a set of questions, answer them 0 for No, 1 for Yes'), nl,
@@ -203,15 +204,15 @@ csss :-
     write('Do you like doing Data Analytics? '), read(Answer6), add_knowledge(data_analyst, Answer6), nl.
 
 check_ece_career :-
-    likes(design_thinking,0),nl,
-    write('See, we understand that you might not feel very confident in design thinking right now. It is a process, a mindset which takes time to develop. However, that mindset is crucial for this field. As well as loving what you do is critical. It is a very fulfilling process or going from ideation to product, making products or services for users so that you can improve their lives. You should try to actually enjoy the process. We can make some recommendations for you to practice these skills.'),nl,
-    write('1. Udemy: Introduction to Graphic Design: Udemy, as most of you already know, is an online learning platform aimed at professionals, catering courses created by experts. The platform is filled with amazing courses whose legibility you can judge by the number of people who have rated/taken the course. '),nl,
-    write('2. Skillshare: Skillshare is another online learning platform, but it also doubles as a community for creative individuals. It offers plenty of graphic design courses, all of which can be readily accessed by a subscription plan. You don’t have to pay for the courses individually, but through a single monthly subscription fee, you get access to all of Skillshares content.'),nl,
-    write('3. Hackdesign.org: Hackdesign offers one of the most complete and structured online course for learning graphic designing. If you want to explore this skill but don’t feel like investing, then you will find Hack Design to offer the perfect solution. You will get access to a number of links and posts, all of which are well structured.'),nl,
-    write('4. DesignLab: DesignLab is an ideal online graphic design learning platform for you. The platform provides users with complete hands-on projects to help them grasp the designing concepts. '), nl.
+    likes(electronics,0),nl,
+    write('See, we understand that you might not feel very confident in this field right now. That is completely alright. However, it is a process. Loving what you do is critical. It is a very wide domain and has so many possibilities and interesting applications. There are tons of research going onon in this field and the possibility to innovate is limitless. You should try to enjoy the process. We can make some recommendations for you to practice these skills.'),nl,
+    write('1. MIT OpenCourseWare: MITOpenCourseWare provides lecture notes, exams, and videos without any required registration. The course best suited for those seeking to learn electrical engineering is Circuits and Electronics, which includes more than 20 video lectures. '),nl,
+    write('2. Collin Cunningham’s MAKE Series: MAKE magazine is on YouTube, bringing DIY to technology. For people who want to learn electrical engineering online, Collin Cunningham’s MAKE Series is a perfect place to start.'),nl,
+    write('3. Electromechanical Library: The Electromechanical Library’s focus is on devices, systems, and applications, and includes a robust collection of interest to people who want to study electrical engineering.'),nl,
+    write('4. Arduino: Arduino is an “open-source electronics prototyping platform based on flexible, easy-to-use hardware and software.” The best part about Arduino is that they provide materials for purchase as well as free online video tutorials so that beginners in electrical engineering can gain hands-on experience and practice.'), nl.
 
 check_ece_career :-
-    likes(design_thinking,1), likes(code, 1), likes(ui, 1), likes(gaming, 0), nl,
+    likes(debug,1), likes(code, 1), likes(complicated, 1), likes(robotics, 0), nl,
     write('You are a good fit for the following careers: '),nl,
     write('1. Graphic designer: These professionals create designs for marketing collateral, product illustrations, brand identities and websites using computer software like the Adobe® Creative Suite. They merge technical skill with artistic ability to create a design that resonates with its desired audience. Graphic designers may work independently as freelancers or contractors, in an in-house setting for a company or in an agency setting.'), nl,
     write('2. UI designer: User interface design is, in fact, a crucial subset of UX—but it’s important to recognize that they are two separate roles. UI design focuses on the user’s visual experience, determining how they interact with the product interface.'), nl,
@@ -238,7 +239,8 @@ ece :-
     write('Do you like designing of controllers, micro-controllers, programmable logic controllers? '), read(Answer4),  add_knowledge(controllers, Answer4), nl,
     write('Do you like analysis, synthesis, and modification of signals? '), read(Answer5), add_knowledge(signals, Answer5), nl,
     /*instrumentation*/
-    write('Do you like physics? '), read(Answer6), add_knowledge(physics, Answer6), nl.
+    write('Do you like physics? '), read(Answer6), add_knowledge(physics, Answer6), nl,
+    write('Do you enjoy Electronics? '), read(Answer7), add_knowledge(electronics, Answer7), nl.
     /*Why Six Sigma for ECE professionals. How does it help? Six Sigma is a customer-centric manufacturing approach to realizing fewer defects and thus lowering costs and increasing customer satisfaction.
 
 The engineering fraternity is greatly benefited from Six Sigma certification as it helps to break down a task into a much-simplified form. It will provide tools and techniques to standardize work methods thus reducing rework and wastage.*/
