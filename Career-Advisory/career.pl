000d98d8154f0a9290ff35d6e9247984dee29823
aman071 <amanrehman18@gmail.com>
2020-09-23 08:15:13 +0530

    added all ques

--- Career-Advisory/career.pl
+++ Career-Advisory/career.pl
@@ -102,3 +102,40 @@ cse :-
     write('Do you like problem solving? '), read(Answer4),  add_knowledge(problem_solving, Answer4), nl,
     write('Do you like working with data? '), read(Answer5), add_knowledge(data_analyst, Answer5), nl,
     write('Do you like Machine Learning? '), read(Answer6), add_knowledge(data_analyst, Answer6), nl.
+
+csd :-
+    write('We will ask you a set of questions, answer them 0 for No, 1 for Yes'), nl,
+    write('Do you like design thinking? '), read(Answer1), add_knowledge(design_thinking, Answer1), nl,
+    write('Do you like coding? '), read(Answer2), add_knowledge(code, Answer2), nl,
+    write('Would you say you have a good imagination? '), read(Answer3),  add_knowledge(imagination, Answer3), nl,
+    write('Would you say you are a creative person? '), read(Answer4),  add_knowledge(creative, Answer4), nl,
+    write('Do you like the domain of user experience? '), read(Answer5), add_knowledge(ux, Answer5), nl,
+    write('Do you like the domain of user interface design? '), read(Answer6), add_knowledge(ui, Answer6), nl,
+    write('Do you like talking to users? '), read(Answer7),  add_knowledge(talk_to_user, Answer7), nl,
+    write('Do you like doing market research? '), read(Answer8), add_knowledge(market_research, Answer8), nl.
+
+
+csss :-
+    write('We will ask you a set of questions, answer them 0 for No, 1 for Yes'), nl,
+    write('Do you like logical reasoning? '), read(Answer1), add_knowledge(logical, Answer1), nl,
+    write('Do you like analytical methods of evaluation? '), read(Answer2), add_knowledge(analytical, Answer2), nl,
+    write('Do you like market research? '), read(Answer3),  add_knowledge(market_research, Answer3), nl,
+    write('Do you like data collection? '), read(Answer4),  add_knowledge(data_collection, Answer4), nl,
+    write('Do you think about solutions from economic, societal, and environment contexts? '), read(Answer5), add_knowledge(context, Answer5),nl,
+    write('Do you like doing Data Analytics? '), read(Answer6), add_knowledge(data_analyst, Answer6), nl.
+
+ece :-
+    write('We will ask you a set of questions, answer them 0 for No, 1 for Yes'), nl,
+    /*desktop suport*/
+    write('Do you like debugging code? '), read(Answer1), add_knowledge(debug, Answer1), nl,
+    write('Do you like coding? '), read(Answer2), add_knowledge(code, Answer2), nl,
+    /*system control, technical advisor */
+    write('Do you like designing of complicated systems but also testing? '), read(Answer3),  add_knowledge(complicated, Answer3), nl,
+    /*robotics*/
+    write('Do you like designing of controllers, micro-controllers, programmable logic controllers? '), read(Answer4),  add_knowledge(controllers, Answer4), nl,
+    write('Do you like analysis, synthesis, and modification of signals? '), read(Answer5), add_knowledge(signals, Answer5), nl,
+    /*instrumentation*/
+    write('Do you like physics? '), read(Answer6), add_knowledge(physics, Answer6), nl.
+    /*Why Six Sigma for ECE professionals. How does it help? Six Sigma is a customer-centric manufacturing approach to realizing fewer defects and thus lowering costs and increasing customer satisfaction.
+
+The engineering fraternity is greatly benefited from Six Sigma certification as it helps to break down a task into a much-simplified form. It will provide tools and techniques to standardize work methods thus reducing rework and wastage.*/
