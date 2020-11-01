e5eb68cb1562704dc2db47e04f1b945d296291d0
aman071 <amanrehman18@gmail.com>
2020-11-01 14:09:17 +0530

    dfs fixed

--- DFS and Best-First/read.pl
+++ DFS and Best-First/read.pl
@@ -25,39 +25,37 @@ remfirst([_|Tail], Tail).
 cities([]).
 cities([H|T], Citynames, Data):-
   remfirst(H, Citynames),
-%  print(Citynames),
-%  nl,
   Data=T.
 
 create_facts([],City,[]).
 create_facts([C1|F], City, [G|H]):-
 %    write("in create_facts"),nl,
-%    write(C1),write(G),nl,
     asserta(edge(C1,City,G)),
+    asserta(edge(City,C1,G)),
     create_facts(F, City, H).
 
 ite(Citynames, []).
 ite(Citynames, [[City|D]|Ta]):-
 %    write("In ITE"),nl,
 %    print(City),nl,
-%    print(D),
+%    print(D),nl,nl,
     create_facts(Citynames,City, D),
 %    write("after create_facts"),nl,
     ite(Citynames, Ta).
 
 get_rows_data:-
+  retractall(goal(_)),
   csv_read_file("D:\\Myinterests\\Sem7\\AI\\Prolog\\DFS and Best-First\\roaddistance1.csv", Rows, []),
   rows_to_lists(Rows, Lists),
   cities(Lists, Citynames, Data),
 %  print(Data),
-  nl,
 %  write("Going to ITE"),nl,
-  ite(Citynames, Data),
+%  write(Citynames),nl, write(Data),
+  ite(Citynames, Data), nl,
   write("checking"),
-  assert(goal('Ahmedabad')),
 %  checkn,
-  solve('Agartala', Sol), nl, write(Sol),
-  nl.
+  assert(goal('Hubli')),
+  solve('Agartala', Sol), nl, write(Sol).
 
 solve(Node, Solution) :-
   dfs([], Node, Solution).
@@ -66,9 +64,11 @@ dfs(Path, Node, [Node|Path]) :-
   goal(Node).
 
 dfs(Path, Node, Sol) :-
-  edge(Neighbor, Node, Cost),
-  Neighbor \=Node,
-  not(member(Neighbor, Path)),
+%  write('hi'),nl,
+  edge(Neighbor, Node, _),
+  Neighbor \= Node,
+  \+ member(Neighbor, Path),
+%  write(Path),nl,
   dfs([Node|Path], Neighbor, Sol).
 
 
